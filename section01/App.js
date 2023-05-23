444b8b4913195da43fb749eef3dc9cc563ad7736
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-24 04:13:04 +0900

    style: Stylesheet

--- section01/App.js
+++ section01/App.js
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text</Text>
+        <Text style={styles.dummyText}>Another piece of text</Text>
       </View>
-      <Text>Hello World!!!!</Text>
+      <Text style={styles.dummyText}>Hello World!!!!</Text>
       <Button title="Click me" />
     </View>
   );
@@ -18,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 20,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 16,
   },
 });
