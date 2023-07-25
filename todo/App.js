a4c35f7c0a220cac0783949a22d2a6850ede9d80
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-25 10:53:39 +0900

    feat: MainScreen - core component 사용 완료

--- todo/App.js
+++ todo/App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello RN</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MainScreen />;
 }
 
 const styles = StyleSheet.create({
--- todo/screens/MainScreen.js
+++ todo/screens/MainScreen.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Text style={styles.pageTitle}>Todo App</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
