31ea6cfc6d2efe71201c290630b0b0546da93b92
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-26 01:55:19 +0900

    feat: React Navigation 적용

--- todo/App.js
+++ todo/App.js
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import MainScreen from './screens/MainScreen';
 
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Main" component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
