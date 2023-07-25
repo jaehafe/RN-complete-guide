8ae7a0dd7c52479980bc2c38c3b049f1161307da
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-25 11:04:48 +0900

    feat: MainScreen StyleSheet 스타일 작성

--- todo/screens/MainScreen.js
+++ todo/screens/MainScreen.js
@@ -1,4 +1,11 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 
 const MainScreen = () => {
@@ -19,4 +26,40 @@ const MainScreen = () => {
 
 export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: Platform.OS === 'android' ? 20 : 0,
+    backgroundColor: '#f7f8fa',
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 54,
+    fontWeight: '600',
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.2)',
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: '500',
+  },
+  emptyListText: {
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    lineHeight: 20,
+    color: '#737373',
+  },
+});
