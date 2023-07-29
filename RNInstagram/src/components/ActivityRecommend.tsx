fdc7324ad43181bb1ff7675c7ef9bda0cab3603f
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-30 01:43:41 +0900

    feat: ActivityRecomment - component UI

--- RNInstagram/src/components/ActivityRecommend.tsx
+++ RNInstagram/src/components/ActivityRecommend.tsx
@@ -1,19 +1,131 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+
 import {IFriendsProfileData} from '../db';
+
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 interface IActivityRecommend {
   data: IFriendsProfileData;
 }
 
 const ActivityRecommend = ({data}: IActivityRecommend) => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+  const [follow, setFollow] = useState(data.follow);
+  const [close, setClose] = useState(false);
+
   return (
     <View>
-      <Text>ActivityRecommend</Text>
+      {close ? null : (
+        <View style={styles.container}>
+          <View>
+            <TouchableOpacity
+              style={styles.userInfoContainer}
+              onPress={() =>
+                navigation.push('FriendProfile', {
+                  name: data.name,
+                  profileImage: data.profileImage,
+                  follow,
+                  post: data.posts,
+                  followers: data.followers,
+                  following: data.following,
+                })
+              }>
+              <Image source={data.profileImage} style={styles.profileImg} />
+              <View style={styles.userInfoWrapper}>
+                <Text style={styles.userName}>{data.name}</Text>
+                <Text style={styles.userAccountName}>{data.accountName}</Text>
+                <Text style={styles.userAccountName}>회원님을 위한 추천</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.wrapper}>
+            {follow ? (
+              <TouchableOpacity
+                onPress={() => setFollow(!follow)}
+                style={{width: follow ? 90 : 68}}>
+                <View style={styles.followButton}>
+                  <Text style={{color: follow ? 'black' : 'white'}}>
+                    {follow ? 'following' : 'follow'}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ) : (
+              <>
+                <TouchableOpacity onPress={() => setFollow(!follow)}>
+                  <View style={styles.followButton}>
+                    <Text style={{color: follow ? 'black' : 'white'}}>
+                      {follow ? 'following' : 'follow'}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.closeButton}
+                  onPress={() => setClose(true)}>
+                  <AntDesign style={styles.closeIcon} name="close" />
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
+      )}
     </View>
   );
 };
 
 export default ActivityRecommend;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userInfoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    maxWidth: '64%',
+  },
+  profileImg: {
+    width: 45,
+    height: 45,
+    borderRadius: 100,
+    marginRight: 10,
+  },
+  userInfoWrapper: {
+    width: '100%',
+  },
+  userName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  userAccountName: {
+    fontSize: 12,
+    opacity: 0.5,
+  },
+  followButton: {
+    width: '100%',
+    height: 30,
+    borderRadius: 5,
+    borderColor: '#DEDEDE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  closeButton: {
+    paddingHorizontal: 10,
+  },
+  closeIcon: {
+    fontSize: 14,
+    color: 'black',
+    opacity: 0.8,
+  },
+});
