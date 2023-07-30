a843f96e1bf6824c59c0bed1f371fcf3231eaf17
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-30 19:06:40 +0900

    feat: FriendProfile - screen header UI

--- RNInstagram/src/screens/FriendProfile.tsx
+++ RNInstagram/src/screens/FriendProfile.tsx
@@ -1,12 +1,62 @@
-import {View, Text} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 
-const FriendProfile = () => {
+import Ionic from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+
+const FriendProfile = ({route, navigation}: any) => {
+  const {name, profileImage, posts, followers, following} = route.params;
+
   return (
-    <View>
-      <Text>FriendProfile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionic name="arrow-back" style={styles.backIcon} />
+        </TouchableOpacity>
+        <View style={styles.profileNameWrapper}>
+          <Text style={styles.profileName}>{name}</Text>
+          <Feather name="more-vertical" style={styles.moreIcon} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default FriendProfile;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  wrapper: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  backIcon: {
+    fontSize: 20,
+    color: 'black',
+  },
+  profileNameWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '92%',
+  },
+  profileName: {
+    fontSize: 15,
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  moreIcon: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
